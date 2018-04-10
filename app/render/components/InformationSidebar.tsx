f1a653b242ca10843c221c2bad9e5c5afd8733a5
ysengrimm <ysengrimm@grimmbart.org>
2018-04-11 00:44:36 +0200

    bump electron version

---
+++ app/render/components/InformationSidebar.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export interface InformationSidebarProps {
+  name: string;
+}
+
+export class InformationSidebar extends React.Component<
+  InformationSidebarProps,
+  {}
+> {
+  constructor(props: InformationSidebarProps) {
+    super(props);
+  }
+
+  render() {
+    return <></>;
+  }
+}
